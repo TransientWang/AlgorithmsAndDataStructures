3519a44d8f1c3fce2359280dcb2cb5e19b8f02c8
wfy815775 <815775754@qq.com>
2018-10-21 14:04:14 +0800

    贪心选择 单源最短路径

--- src/test/greedychoice/ActivitySelection.java
+++ src/test/greedychoice/ActivitySelection.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 /**
  * @author wangfy
- * @Description TODO
+ * @Description 贪心选择
  * @date 2018/10/15
  */
 
@@ -54,5 +54,89 @@ public class ActivitySelection {
             }
         }
         System.out.println(k + 1);
+    }
+
+
+    @Test
+    public void Tests() {
+        int[][] map = {{1000, 2, 5, 1000, 1000},
+                {1000, 1000, 2, 6, 1000},
+                {1000, 1000, 1000, 7, 1},
+                {1000, 1000, 2, 1000, 4},
+                {1000, 1000, 1000, 1000, 1000}};
+        dijkstraSolution(map);
+
+    }
+
+    /**
+     * @return void
+     * @Description 单元最短路径
+     * map代表有向图 从i到j的权重
+     * @Param [map]
+     * @Line 64
+     **/
+    public void dijkstraSolution(int[][] map) {
+        //原点到各个点的距离
+        int[] dist = new int[map[0].length];
+        //各个点的前驱节点
+        int[] p = new int[dist.length];
+        //为true是代表该节点已经走过
+        boolean[] flag = new boolean[p.length];
+
+        //初始化
+        for (int i = 0; i < p.length; i++) {
+            dist[i] = map[0][i];
+            flag[i] = false;
+            if (dist[i] == 1000) {
+                p[i] = -1;
+            } else {
+                p[i] = 0;
+            }
+        }
+        flag[0] = true;
+        p[0] = 0;
+
+        //做出总结点-1次选择（原点不算）
+        for (int i = 1; i < p.length; i++) {
+            //筛选出最短路径的辅助变量
+            int temp = 1000;
+            //最短路径所在的节点
+            int t = 0;
+            //从未走过路径中选择dist[]最小的的顶点t
+            for (int j = 1; j < p.length; j++) {
+                //如果此节点还未走过而且到此节点的最短路径小于temp
+                if (!flag[j] && dist[j] < temp) {
+                    temp = dist[j];
+                    t = j;
+                }
+            }
+            //
+            flag[t] = true;
+            for (int j = 1; j < p.length; j++) {
+                //如果此节点没走过而且从t到j可以达到
+                if (!flag[j] && map[t][j] < 1000) {
+                    //如果当前到j节点的最短路径比从t开始到j的最短路径大
+                    if (dist[j] > (dist[t] + map[t][j])) {
+                        //将j节点的最短距离更新
+                        dist[j] = dist[t] + map[t][j];
+                        //将达到j节点的前驱节点设为t
+                        p[j] = t;
+                    }
+                }
+            }
+
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println();
+        //根据p找出应走路径
+        int index=4;
+        while (index>0){
+            stringBuilder.append(String.valueOf(index + 1));
+            stringBuilder.append("-");
+            index = p[index];
+        }
+        stringBuilder.append(String.valueOf(index + 1));
+        System.out.println(stringBuilder);
+
     }
 }
