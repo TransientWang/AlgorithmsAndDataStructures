97d8255679cb9963d1cbb198d8987132759f335a
wfy815775 <815775754@qq.com>
2018-10-28 20:16:31 +0800

    动态规划 不同路径（有障碍情况）

--- src/test/dynamicProgramming/dynamicTest.java
+++ src/test/dynamicProgramming/dynamicTest.java
@@ -337,7 +337,10 @@ public class dynamicTest {
 
     @Test
     public void tests() {
-        uniquePaths1(1, 2);
+//        uniquePaths1(1, 2);
+        int[][] path = {{0, 1, 0}, {0, 1, 0}, {0, 0, 0}};
+        int[][] pa = {{1, 0}, {0, 0}};
+        System.out.println(uniquePathsWithObstacles(pa));
     }
 
     /**
@@ -374,7 +377,7 @@ public class dynamicTest {
         path[1][0] = 1;
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                    path[i][j] = path[i - 1][j] + path[i][j - 1];
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
 
             }
         }
@@ -385,5 +388,81 @@ public class dynamicTest {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * @Description 路径问题   输入有1代表此路不通
+     * @Param [obstacleGrid]
+     * @return int
+     * @Line 407
+     **/
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = -1;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(obstacleGrid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        if (obstacleGrid[0][0] ==-1){
+            return 0;
+        }
+        if (m == 1 || n == 1) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (obstacleGrid[i][j] == -1) {
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] != -1)
+                obstacleGrid[i][0] = 1;
+            else {
+                obstacleGrid[i][0] = 0;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] != -1)
+                obstacleGrid[0][i] = 1;
+            else {
+                obstacleGrid[0][i] = 0;
+                break;
+            }
+        }
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int x = obstacleGrid[i - 1][j] == -1 ? 0 : obstacleGrid[i - 1][j];
+                int y = obstacleGrid[i][j - 1] == -1 ? 0 : obstacleGrid[i][j - 1];
+                if (obstacleGrid[i][j] != -1)
+                    obstacleGrid[i][j] = x + y;
+                else
+                    obstacleGrid[i][j] = 0;
+            }
+        }
+
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(obstacleGrid[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        return obstacleGrid[m - 1][n - 1];
     }
 }
