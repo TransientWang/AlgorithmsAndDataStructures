53c11a86efa49fce0773df18ad36a54e503982c1
wfy815775 <815775754@qq.com>
2018-10-29 17:05:19 +0800

    分治法  寻找众数

--- src/test/divideAndConquer/test.java
+++ src/test/divideAndConquer/test.java
@@ -34,7 +34,7 @@ public class test {
      * 如果等于cur位置，说明哨兵就是第k小的值
      * 否则，第k小的元素一定在哨兵右边里面，为 第k-哨兵长度个
      * 则可以递归调用来寻找值
-     *
+     * <p>
      * 注意，k是从1开始的
      * @Param [a, left, right, k]
      * k代表第k小的数
@@ -60,16 +60,16 @@ public class test {
         a[left] = a[j];
         a[j] = cur;
         //先进行了常规的插入排序
-        System.out.print("结果 " + left + "-" + right+":");
+        System.out.print("结果 " + left + "-" + right + ":");
         Arrays.stream(a).forEachOrdered(var -> System.out.print(var + " "));
         System.out.println();
         //接下来是判断第K小个数在哪里
-        if (left+k == j + 1) {
+        if (left + k == j + 1) {
             //第一种请况，判断K是否与哨兵相等，相等的话就返回哨兵的位置
             //不相等的话那么K就在哨兵的左边或者右边了
             //由于K是从1开始的，哨兵是从0开始的，所以把哨兵数加一
             return a[j];
-        } else if (left+k <= j) {
+        } else if (left + k <= j) {
             //如果k在哨兵的左边，也就是说明锁边的数量一定大于
             // k，这是对左边进行插入排序则可以找到第k小的数
 //            return insertSort(a, left, j, k);
@@ -108,4 +108,47 @@ public class test {
         System.out.println();
         return a[left + k - 1];
     }
+
+    @Test
+    public void test1() {
+        int[] arrs = {3,2,3};
+        System.out.println(majorityElement(arrs));
+
+    }
+
+
+    /**
+     * @return int
+     * @Description 众数
+     * 摩尔投票法  利用了分治法的思想
+     * 假定第一个数为所找的众数
+     * 然后定义一个计数器  用来投票用
+     * 因为现在 已经有了一个 众数  那么 这个投票器的初始值就应该为1
+     * 继续向下遍历  遇到相同的计数器 +1  不同的计数器-1
+     * 当计数器为0的时候 代表前面所遍历过得的数字被分成了相等的两部分 其中众数占了一半
+     * 这时候就要 假定当前索引位置的数字是众数 然后重复上述的过程
+     * 因为众数的数量大于数组的一半，所以到最后假定的众数就是我们要找的众数
+     * @Param [nums]
+     * @Line 117
+     **/
+    public int majorityElement(int[] nums) {
+        int count =1;
+        int num = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (num == nums[i]){
+                count++;
+            }else {
+                count --;
+            }
+            if (count ==0){
+                num = nums[i];
+                count = 1;
+            }
+        }
+        return num;
+    }
+
+
+
+
 }
