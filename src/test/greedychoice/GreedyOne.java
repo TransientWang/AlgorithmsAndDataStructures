b74a02386bf77f5ae04e892beb2e819b8988c5ec
wfy815775 <815775754@qq.com>
2018-10-31 23:24:12 +0800

    z

---
+++ src/test/greedychoice/GreedyOne.java
@@ -0,0 +1,25 @@
+package test.greedychoice;
+
+import org.junit.Test;
+
+/**
+ * @author wangfy
+ * @Description TODO
+ * @date 2018/10/31
+ */
+
+public class GreedyOne {
+
+    @Test
+    public void hh(){
+        System.out.println(canJump(new int[]{2,0}));
+    }
+    public boolean canJump(int[] nums) {
+
+
+
+
+
+        return false;
+    }
+}
