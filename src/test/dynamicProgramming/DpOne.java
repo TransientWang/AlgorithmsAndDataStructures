fd58fec20b1330be5bb7e39e7f484ce923b46422
wfy815775 <815775754@qq.com>
2018-11-02 14:14:24 +0800

    动态规划 解码方法

--- src/test/dynamicProgramming/DpOne.java
+++ src/test/dynamicProgramming/DpOne.java
@@ -100,14 +100,14 @@ public class DpOne {
     @Test
     public void testOne() {
 
-        out.println(numDecodings("10101"));
+        out.println(numDecodings("210"));
 
     }
     /**
      * @Description TODO   没想明白
      * 这道题要求解码方法，跟之前那道 Climbing Stairs 爬梯子问题 非常的相似，但是还有一些其他的限制条件，
      * 比如说一位数时不能为0，两位数不能大于26，其十位上的数也不能为0，出去这些限制条件，根爬梯子基本没啥区别，
-     * 也勉强算特殊的斐波那契数列，当然需要用动态规划Dynamci Programming来解。建立一位dp数组，长度比输入数组长多多2，
+     * 也勉强算特殊的斐波那契数列，当然需要用动态规划Dynamci Programming来解。建立一位dp数组，长度比输入数组长多2，
      * 全部初始化为1，因为斐波那契数列的前两项也为1，然后从第三个数开始更新，对应数组的第一个数。对每个数组首先判断其是否为0，
      * 若是将改为dp赋0，若不是，赋上一个dp值，此时相当如加上了dp[i - 1], 然后看数组前一位是否存在，如果存在且满足前一位不是0，
      * 且和当前为一起组成的两位数不大于26，则当前dp值加上dp[i - 2], 至此可以看出来跟斐波那契数组的递推式一样，代码如下：
@@ -118,11 +118,19 @@ public class DpOne {
      * @Line 110
      **/
     public int numDecodings(String s) {
+        //如果s[k] != 0那么他就等于 s[k-1]s[k] +s[k-2]s[k]  也就是前两种情况的组合 。
+        //如果s[k] ==0 那么他只能和s[k-1]组成一位编码数，也就是s[k-1]s[k]已经是固定的了 所以s[k] =s[k-2]s[k]
+        //即s[k]先置为0然后在加上s[k-2]的数量 ，如果s[k] != 0 那么 如果他不是第二位（）s[k]就等于
         if (s.isEmpty() || (s.length() > 1 && s.charAt(0) == '0')) return 0;
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
-        for (int i = 1; i < dp.length; ++i) {        //dp[i-1] ==0 的时候 dp[i] = dp[i-2]
-            dp[i] = (s.charAt(i - 1) == '0') ? 0 : dp[i - 1];
+        for (int i = 1; i < dp.length; i++) {        // 注意：是dp.length
+            if (s.charAt(i - 1) == '0'){
+                dp[i] =0;
+            }else {
+                dp[i] = dp[i-1];
+            }
+//            dp[i] = (s.charAt(i - 1) == '0') ? 0 : dp[i - 1];
             if (i > 1 && (s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6'))) {
                 dp[i] += dp[i - 2];
             }
