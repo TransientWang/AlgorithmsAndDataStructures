5251ecf614a774a0f24ed2c387c050b20996abe4
wfy815775 <815775754@qq.com>
2018-11-08 12:16:09 +0800

    排序 数组合并

--- src/wfy/sort/test1.java
+++ src/wfy/sort/test1.java
@@ -7,9 +7,10 @@ import java.util.Random;
 
 public class test1 {
     @Test
-    public void aaa(){
+    public void aaa() {
         System.out.println(16 | 16);
     }
+
     @Test
     public void aaaa() {
         Random r = new Random(30);
@@ -48,34 +49,34 @@ public class test1 {
     }
 
 
-
-    public static void insert(int a[]){
+    public static void insert(int a[]) {
         for (int i = 1; i < a.length; i++) {
             int temp = a[i];
-            for (int j = (i-1); j >= 0; j--) {
-                if (temp < a[j]){
-                    a[j+1] = a[j];
-                      a[j] = temp;
+            for (int j = (i - 1); j >= 0; j--) {
+                if (temp < a[j]) {
+                    a[j + 1] = a[j];
+                    a[j] = temp;
                 }
             }
         }
     }
 
     public static void shellinsert(int a[]) {
-        int h = a.length/4;
+        int h = a.length / 4;
         while (h >= 1) {
             for (int i = h; i < a.length; i++) {
                 int temp = a[i];
-                for (int j = (i - h); j >=0; j--) {
+                for (int j = (i - h); j >= 0; j--) {
                     if (temp > a[j]) {
                         a[j + 1] = a[j];
                         a[j] = temp;
                     }
                 }
-                h/=2;
+                h /= 2;
             }
         }
     }
+
     //判断条件只能是 i j
     public static void sort(int[] a, int left, int riggt) {
         if (left > riggt)
@@ -104,6 +105,43 @@ public class test1 {
 
     }
 
+    /**
+     * @return void
+     * @Description 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+     * <p>
+     * 说明:
+     * <p>
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+     * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+     * @Param [nums1, m, nums2, n]
+     * @Line 110
+     **/
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        while (m < nums1.length && i < m) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                int t = m - 1;
+                while (i <= t) {
+                    nums1[t + 1] = nums1[t];     //插入排序部分 要从后面往前面遍历，比较方便，不会出现当数组长度短的时候 索引溢出的情况
+                    t--;
+                }
+                nums1[i] = nums2[j++];
+                m++;
+                i++;
+            }
+        }
+        while (j < n) {
+            nums1[m++] = nums2[j++];
+        }
+        Arrays.stream(nums1).forEachOrdered(o -> System.out.print(o + " "));
+    }
 
+    @Test
+    public void test() {
+        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
 }
