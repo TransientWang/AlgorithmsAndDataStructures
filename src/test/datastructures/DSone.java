f6a64c0b9b326344052a41ca4e284457f0a28652
wfy815775 <815775754@qq.com>
2019-03-20 12:04:12 +0800

    375. 猜数字大小 II

--- src/test/datastructures/DSone.java
+++ src/test/datastructures/DSone.java
@@ -1,5 +1,7 @@
 package test.datastructures;
 
+import org.junit.Test;
+
 import java.util.*;
 
 /**
@@ -39,31 +41,56 @@ public class DSone {
     }
 
     /**
-     * @date 2019/3/20 9:37
      * @return test.datastructures.TreeNode
+     * @date 2019/3/20 9:37
      * @Description 236. 二叉树的最近公共祖先
      * @Param [root, p, q]
      **/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p == root || q == root){
+        if (p == root || q == root) {
             return p == root ? p : q;
         }
         TreeNode left = null;
         TreeNode right = null;
-        if(root.left != null){
-            left = lowestCommonAncestor(root.left,p,q);
+        if (root.left != null) {
+            left = lowestCommonAncestor(root.left, p, q);
         }
-        if(root.right != null){
-            right = lowestCommonAncestor(root.right,p,q);
+        if (root.right != null) {
+            right = lowestCommonAncestor(root.right, p, q);
         }
 
 
-
-        if(left != null && right != null){
+        if (left != null && right != null) {
             return root;
         }
-        return left == null ? right:left;
+        return left == null ? right : left;
 
+    }
+
+    /**
+     * @date 2019/3/20 12:04
+     * @return int
+     * @Description 375. 猜数字大小 II
+     * @Param [n]
+     **/
+    public int getMoneyAmount(int n) {
+
+        int dp[][] = new int[n + 1][n + 1];
+        for (int lo = n - 1; lo >= 1; lo--) {
+            for (int hi = lo + 1; hi <= n; hi++) {
+                dp[lo][hi] = Integer.MAX_VALUE;
+                for (int idx = lo; idx < hi; idx++) {
+                    System.out.format("%d,%d\n",lo,hi);
+                    dp[lo][hi] = Math.min(dp[lo][hi], idx + Math.max(dp[lo][idx - 1], dp[idx + 1][hi]));
+                }
+            }
+        }
+        return dp[1][n];
+    }
+
+    @Test
+    public void test() {
+        getMoneyAmount(10);
     }
 
 }
