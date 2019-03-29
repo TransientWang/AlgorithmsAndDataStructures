09666d08c4131e80538aebbc30725bd7f3ab3e62
wfy815775 <815775754@qq.com>
2019-03-29 09:49:29 +0800

    213. 打家劫舍 II(reveiw)

--- src/test/datastructures/DSTwo.java
+++ src/test/datastructures/DSTwo.java
@@ -66,21 +66,34 @@ public class DSTwo {
     public int rob(int[] nums) {
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
-        if (nums.length == 3) return Math.max(Math.max(nums[0], nums[1]), nums[2]);
-        int[] dp1 = new int[nums.length];
-        int[] dp2 = new int[nums.length];
-        dp1[0] = nums[0];
-        dp1[1] = Math.max(nums[0], nums[1]);
-        dp2[1] = nums[1];
-        dp2[2] = Math.max(nums[1], nums[2]);
-        for (int i = 2; i < dp1.length - 1; i++) {
-            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+//        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+//        if (nums.length == 3) return Math.max(Math.max(nums[0], nums[1]), nums[2]);
+//        int[] dp1 = new int[nums.length];
+//        int[] dp2 = new int[nums.length];
+//        dp1[0] = nums[0];
+//        dp1[1] = Math.max(nums[0], nums[1]);
+//        dp2[1] = nums[1];
+//        dp2[2] = Math.max(nums[1], nums[2]);
+//        for (int i = 2; i < dp1.length - 1; i++) {
+//            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+//        }
+//        for (int i = 3; i < dp2.length; i++) {
+//            dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1]);
+//        }
+//        return Math.max(dp1[nums.length - 2], dp2[nums.length - 1]);
+        return Math.max(robHelp(nums, 1, nums.length), robHelp(nums, 0, nums.length - 1));
+
+    }
+
+    public int robHelp(int[] nums, int left, int right) {
+        if (right - left == 1) return nums[left];
+        int[] dp = new int[nums.length];
+        dp[left] = nums[left];
+        dp[left + 1] = Math.max(nums[left + 1], nums[left]);
+        for (int i = left + 2; i < right; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        for (int i = 3; i < dp2.length; i++) {
-            dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1]);
-        }
-        return Math.max(dp1[nums.length - 2], dp2[nums.length - 1]);
+        return dp[right - 1];
     }
 
     @Test
